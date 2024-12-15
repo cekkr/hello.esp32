4af4bdc2e83e7a40c2bc3ec5a2f2c7bb52bfe221
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-15 08:47:54 +0100

    hero

--- hello-idf/build.sh
+++ hello-idf/build.sh
@@ -38,7 +38,7 @@ cd "$CUR_DIR"
 #idf.py set-target esp32
 #idf.py menuconfig
 
-idf.py build -DCMAKE_BUILD_TYPE=Debug -DESP_SYSTEM_INIT_DEBUG_INFO=y #-DCMAKE_C_FLAGS="-H" 2>&1 | tee build_output.txt
+idf.py build #-DCMAKE_BUILD_TYPE=Debug -DESP_SYSTEM_INIT_DEBUG_INFO=y #-DCMAKE_C_FLAGS="-H" 2>&1 | tee build_output.txt
 #idf.py build -DCMAKE_BUILD_TYPE=Release
 #idf.py build -DCMAKE_BUILD_TYPE=Release -DESP_SYSTEM_INIT_DEBUG_INFO=y
 
