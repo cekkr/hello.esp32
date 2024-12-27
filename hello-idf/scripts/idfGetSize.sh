44be5618f5c169bfd3916ca97acbbb214117930f
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-27 23:01:00 +0100

    test

--- hello-idf/scripts/idfGetSize.sh
+++ hello-idf/scripts/idfGetSize.sh
@@ -12,6 +12,10 @@ source ./espShellEnv.sh
 #idf.py menuconfig
 #idf.py build #-DCMAKE_C_FLAGS="-H" 2>&1 | tee build_output.txt
 
+#idf.py build
 idf.py size-files
 
-#xtensa-esp32-elf-nm -S --size-sort build/nome_progetto.elf | grep -i iram
+#xtensa-esp32-elf-nm -S --size-sort build/hello-idf.elf | grep -i iram
+#xtensa-esp32-elf-nm -S -l build/hello-idf.elf | sort -n
+
+#xtensa-esp32-elf-objdump -x -d build/esp-idf/main/CMakeFiles/__idf_main.dir/wasm3/source/m3_compile.c.obj > memory_analysis.txt
