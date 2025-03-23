4c0532e60390d2357337d0f69af6570159a95689
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-03-23 03:43:11 +0100

    welcome back to the hell

--- hello-wasm/samples/continueMalloc.c
+++ hello-wasm/samples/continueMalloc.c
@@ -1,54 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <emscripten.h>
-
 #include "../bindings/esp_wasm.h"
 
-#define TOTAL_INTEGERS 1000000
-#define PRINT_INTERVAL 1024
-
-// Funzione per stampare gli interi generati fino ad ora
-void print_integers(int* array, int count) {
-    printf("=== Stampando %d interi ===\n", count);
-    for (int i = 0; i < count; i++) {
-        printf("Posizione: %d, Valore: %d\n", i, array[i]);
-    }
-    printf("=== Fine stampa ===\n\n");
-}
-
-// Funzione principale per la generazione di interi
+// Funzione principale
 int start() {
-    // Inizializzazione del generatore di numeri casuali
-    srand(time(NULL));
+    int numeri[10000];
     
-    // Allocazione dell'array per memorizzare gli interi
-    int* integers = (int*)malloc(TOTAL_INTEGERS * sizeof(int));
-    if (integers == NULL) {
-        printf("Errore di allocazione memoria\n");
-        return 1;
-    }
-    
-    // Generazione di interi casuali
-    printf("Inizio generazione di %d interi...\n", TOTAL_INTEGERS);
-    
-    for (int i = 0; i < TOTAL_INTEGERS; i++) {
-        // Generazione di un intero casuale
-        integers[i] = rand();
+    // Riempimento dell'array con numeri
+    for (int i = 0; i < 10000; i++) {
+        numeri[i] = i;  // Assegna il valore dell'indice
         
-        // Stampa ogni PRINT_INTERVAL interi
-        if ((i + 1) % PRINT_INTERVAL == 0 || i == TOTAL_INTEGERS - 1) {
-            print_integers(integers, i + 1);
-            
-            // Per Emscripten, aggiungiamo un'opportunità di aggiornare l'UI
-            emscripten_sleep(0);
+        // Stampa l'array ogni 100 numeri
+        if ((i + 1) % 100 == 0) {
+            printf("Dopo %d elementi:\n", i + 1);
+            // Stampa tutti i numeri fino ad ora
+            for (int j = 0; j <= i; j++) {
+                printf("%d ", numeri[j]);
+            }
+            printf("\n\n");
         }
     }
-    
-    printf("Generazione completata!\n");
-    
-    // Liberazione della memoria
-    free(integers);
     
     return 0;
 }
